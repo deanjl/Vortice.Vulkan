42202265f1b110f4ef45637a8bc088b607b72786
Dean J Lee <deanjlee@protonmail.com>
2025-03-16 09:18:10 +1100

    Initialized Vulkan.

--- src/samples/FSharp/Program.fs
+++ src/samples/FSharp/Program.fs
@@ -1,2 +1,14 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open Vortice.Vulkan
+open type Vortice.Vulkan.Vulkan
+
+(* utils *)
+
+/// Check the given Vulkan operation result, logging on non-Success.
+let check (result : VkResult) =
+    if int result > 0 then printfn "Vulkan info: %s" (string result)
+    elif int result < 0 then printfn "Vulkan assertion failed due to: %s" (string result)
+
+(* main program *)
+
+// load vulkan
+vkInitialize () |> check

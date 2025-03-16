261aed5c3e8cef2db35a08b1cf6512b5ebe7bdaa
Dean J Lee <deanjlee@protonmail.com>
2025-03-16 14:45:11 +1100

    Implemented manual memory allocation and reproduced error.

--- src/samples/FSharp/Program.fs
+++ src/samples/FSharp/Program.fs
@@ -21,6 +21,15 @@ let asPointer<'a when 'a : unmanaged> (managedPtr : byref<'a>) : nativeptr<'a> =
     let voidPtr = Unsafe.AsPointer<'a> &managedPtr
     NativePtr.ofVoidPtr<'a> voidPtr
 
+/// Convert a fixed-size buffer to an array of given length.
+let fixedBufferToArray<'a when 'a : unmanaged> length fixedBuffer =
+    let mutable fixedBuffer = fixedBuffer
+    let voidPtr = Unsafe.AsPointer &fixedBuffer
+    let ptr = NativePtr.ofVoidPtr<'a> voidPtr
+    let array = Array.zeroCreate<'a> length
+    for i in 0 .. (length - 1) do array.[i] <- NativePtr.get ptr i
+    array
+
 /// Abstraction for native pointer pinning for arrays.
 type ArrayPin<'a when 'a : unmanaged> private (handle : Buffers.MemoryHandle, ptr : nativeptr<'a>) =
 
@@ -44,6 +53,84 @@ type ArrayPin<'a when 'a : unmanaged> private (handle : Buffers.MemoryHandle, pt
 let check (result : VkResult) =
     if int result > 0 then printfn "Vulkan info: %s" (string result)
     elif int result < 0 then printfn "Vulkan assertion failed due to: %s" (string result)
+
+/// A manually allocated buffer for diagnostic purposes.
+type ManualAllocatedBuffer =
+    { Buffer : VkBuffer 
+      Memory : VkDeviceMemory
+      Mapping : nativeptr<voidptr> }
+
+    static member private findMemoryType typeFilter properties =
+        
+        // get memory types
+        let mutable memProperties = Unchecked.defaultof<VkPhysicalDeviceMemoryProperties>
+        vkGetPhysicalDeviceMemoryProperties (physicalDevice, &memProperties)
+        let memoryTypes = fixedBufferToArray<VkMemoryType> (int memProperties.memoryTypeCount) memProperties.memoryTypes
+
+        // try find suitable memory type
+        let mutable memoryTypeOpt = None
+        for i in 0 .. (memoryTypes.Length - 1) do
+            match memoryTypeOpt with
+            | None -> if typeFilter &&& (1u <<< i) <> 0u && memoryTypes.[i].propertyFlags &&& properties = properties then memoryTypeOpt <- Some (uint i)
+            | Some _ -> ()
+
+        // fin
+        match memoryTypeOpt with
+        | Some memoryType -> memoryType
+        | None -> printfn "Failed to find suitable memory type!"; 0u
+    
+    static member private createInternal uploadEnabled bufferInfo =
+
+        // create buffer
+        let mutable buffer = Unchecked.defaultof<VkBuffer>
+        vkCreateBuffer (device, &bufferInfo, nullPtr, asPointer &buffer) |> check
+
+        // get buffer memory requirements
+        let mutable memRequirements = Unchecked.defaultof<VkMemoryRequirements>
+        vkGetBufferMemoryRequirements (device, buffer, &memRequirements)
+
+        // choose appropriate memory properties
+        let properties =
+            if uploadEnabled then VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT ||| VK_MEMORY_PROPERTY_HOST_COHERENT_BIT
+            else VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT
+
+        // allocate memory
+        let mutable info = VkMemoryAllocateInfo ()
+        info.allocationSize <- memRequirements.size
+        info.memoryTypeIndex <- ManualAllocatedBuffer.findMemoryType memRequirements.memoryTypeBits properties
+        let mutable memory = Unchecked.defaultof<VkDeviceMemory>
+        vkAllocateMemory (device, asPointer &info, nullPtr, &memory) |> check
+
+        // bind buffer to memory
+        vkBindBufferMemory (device, buffer, memory, 0UL) |> check
+
+        // map memory if upload enabled
+        let mutable mapping = nullPtr
+        if uploadEnabled then vkMapMemory (device, memory, 0UL, VK_WHOLE_SIZE, VkMemoryMapFlags.None, mapping) |> check
+        
+        // make ManualAllocatedBuffer
+        let manualAllocatedBuffer = 
+            { Buffer = buffer
+              Memory = memory
+              Mapping = mapping }
+
+        // fin
+        manualAllocatedBuffer
+
+    /// Create a manually allocated uniform buffer.
+    static member createUniform size =
+        let mutable info = VkBufferCreateInfo ()
+        info.size <- uint64 size
+        info.usage <- Vulkan.VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT
+        info.sharingMode <- Vulkan.VK_SHARING_MODE_EXCLUSIVE
+        let allocatedBuffer = ManualAllocatedBuffer.createInternal true info
+        allocatedBuffer
+    
+    /// Destroy a ManualAllocatedBuffer.
+    static member destroy buffer =
+        if buffer.Mapping <> nullPtr then Vulkan.vkUnmapMemory (device, buffer.Memory)
+        Vulkan.vkDestroyBuffer (device, buffer.Buffer, nullPtr)
+        Vulkan.vkFreeMemory (device, buffer.Memory, nullPtr)
 
 (* main functions *)
 
@@ -85,3 +172,6 @@ createDevice ()
 
 // load device commands
 vkLoadDevice device
+
+// create and allocate buffer
+let buffer = ManualAllocatedBuffer.createUniform 1024
